40f3b22a5aece421b3d882d17cf678de99f63ed1
Ivan Elfimov <ielfimov@gmail.com>
2021-10-02 18:51:35 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,361 @@
+package main
+
+import (
+	"bytes"
+	"embed"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+//go:embed static
+var static embed.FS
+
+//go:embed index.html
+var indexHTML string
+
+var kvconn redis.Conn
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+
+	// Client ID Header for WebSocket events
+	wsClientIdHeader = "x-ws-client-id"
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	kvconn, err := redis.DialURL("redis://localhost:6379")
+	if err != nil {
+		log.Fatalf("Unable to connect to kv store: %v", err)
+	}
+	defer kvconn.Close()
+	hub := newHub()
+	go hub.run()
+	h := Handlers{kv: kvconn, hub: hub}
+
+	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.FileServer(http.FS(static)))
+	r.HandleFunc("/", IndexHandler)
+	r.HandleFunc("/items", h.ItemsHandler)
+	r.HandleFunc("/additem", h.AddItemHandler)
+	r.HandleFunc("/deleteitem", h.DeleteItemHandler)
+	r.HandleFunc("/edititem", h.EditItemHandler)
+	r.HandleFunc("/toggleitem", h.ToggleItemHandler)
+	r.HandleFunc("/ws", func(rw http.ResponseWriter, r *http.Request) {
+		serveWS(hub, rw, r)
+	})
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, r)))
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	remoteUser := r.Header.Get("Remote-User")
+	if remoteUser == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	w.Write([]byte(indexHTML))
+}
+
+type Handlers struct {
+	kv  redis.Conn
+	hub *Hub
+}
+
+type Item struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Category  string `json:"category"`
+	IsChecked bool   `json:"is_checked"`
+}
+
+func (h *Handlers) ItemsHandler(w http.ResponseWriter, r *http.Request) {
+	itemKeys, err := redis.ByteSlices(h.kv.Do("KEYS", "item:*"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	items := make([]Item, 0)
+	for _, itemKey := range itemKeys {
+		var item Item
+		result, _ := redis.Bytes(h.kv.Do("GET", itemKey))
+		err = json.Unmarshal(result, &item)
+		if err != nil {
+			continue
+		}
+		items = append(items, item)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(items)
+}
+
+func (h *Handlers) AddItemHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.Header.Get("Remote-User")
+	if username != "" {
+		// find user and add username + id to the item
+	}
+
+	itemKeys, err := redis.ByteSlices(h.kv.Do("KEYS", "item:*"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	maxID := 1
+	for _, itemKey := range itemKeys {
+		itemKeyParts := strings.Split(string(itemKey), ":")
+		id, _ := strconv.Atoi(itemKeyParts[1])
+		if id > maxID {
+			maxID = id
+		}
+	}
+	newID := maxID + 1
+	name := r.URL.Query().Get("name")
+	category := r.URL.Query().Get("category")
+
+	item := Item{
+		ID:        newID,
+		Name:      name,
+		Category:  category,
+		IsChecked: false,
+	}
+	data, _ := json.Marshal(item)
+	h.kv.Do("SET", fmt.Sprintf("item:%d", newID), data)
+	w.Write(data)
+
+	msg, _ := json.Marshal(Message{ClientID: r.Header.Get(wsClientIdHeader), Type: "add", Data: item})
+	h.hub.broadcast <- msg
+}
+
+func (h *Handlers) DeleteItemHandler(w http.ResponseWriter, r *http.Request) {
+	itemID := r.URL.Query().Get("id")
+	if itemID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	key := fmt.Sprintf("item:%s", itemID)
+	itemRaw, _ := redis.Bytes(h.kv.Do("GET", key))
+	if len(itemRaw) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	var item Item
+	_ = json.Unmarshal(itemRaw, &item)
+	_, err := h.kv.Do("DEL", fmt.Sprintf("item:%s", itemID))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	msg, _ := json.Marshal(Message{ClientID: r.Header.Get(wsClientIdHeader), Type: "delete", Data: item})
+	h.hub.broadcast <- msg
+}
+
+func (h *Handlers) EditItemHandler(w http.ResponseWriter, r *http.Request) {
+	itemID := r.URL.Query().Get("id")
+	if itemID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	name := r.URL.Query().Get("name")
+	category := r.URL.Query().Get("category")
+	key := fmt.Sprintf("item:%s", itemID)
+	itemRaw, _ := redis.Bytes(h.kv.Do("GET", key))
+	if len(itemRaw) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	var item Item
+	_ = json.Unmarshal(itemRaw, &item)
+	item.Name = name
+	item.Category = category
+	updatedItem, _ := json.Marshal(item)
+	h.kv.Do("SET", key, updatedItem)
+
+	msg, _ := json.Marshal(Message{ClientID: r.Header.Get(wsClientIdHeader), Type: "edit", Data: item})
+	h.hub.broadcast <- msg
+
+}
+
+func (h *Handlers) ToggleItemHandler(w http.ResponseWriter, r *http.Request) {
+	itemID := r.URL.Query().Get("id")
+	if itemID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	key := fmt.Sprintf("item:%s", itemID)
+	itemRaw, _ := redis.Bytes(h.kv.Do("GET", key))
+	if len(itemRaw) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	var item Item
+	_ = json.Unmarshal(itemRaw, &item)
+	item.IsChecked = !item.IsChecked
+	updatedItem, _ := json.Marshal(item)
+	h.kv.Do("SET", key, updatedItem)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(updatedItem)
+
+	// Notify all connectede clients
+	msg, _ := json.Marshal(Message{ClientID: r.Header.Get(wsClientIdHeader), Type: "toggle", Data: item})
+	h.hub.broadcast <- msg
+}
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			var msg Message
+			_ = json.Unmarshal(message, &msg)
+			for client := range h.clients {
+				if client.id == msg.ClientID {
+					continue
+				}
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+type Message struct {
+	ClientID string `json:"client_id"`
+	Type     string `json:"type"`
+	// Data has to be a marshalable json struct
+	Data interface{} `json:"data"`
+}
+
+type Client struct {
+	id   string
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.hub.broadcast <- message
+	}
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func serveWS(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	clientID := r.URL.Query().Get("client_id")
+	client := &Client{id: clientID, hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	go client.writePump()
+	go client.readPump()
+}
---
+++ static/script.js
@@ -0,0 +1,256 @@
+let loc = window.location;
+var scheme = 'ws';
+if (loc.protocol === 'https:') {
+  scheme = 'wss';
+}
+const clientID = Math.floor(100000 + Math.random() * 900000)
+let socket = new WebSocket(`${scheme}://${loc.host}/ws?client_id=${clientID}`)
+
+const app = Vue.createApp({
+  data() {
+    return {
+      loading: true,
+      isModalShown: false,
+      modalTitle: "",
+      modalType: "",
+      searchText: "",
+      hideCompleted: false,
+      isGrouped: true,
+      items: [],
+      editItemMode: "add",
+      editItemId: null,
+      editItemName: "",
+      editItemCategory: "",
+      editItemError: "",
+      suggestedCategories: []
+    }
+  },
+  computed: {
+    searchIsEmpty() {
+      return this.searchText !== "" && this.filteredItems.length === 0
+    },
+    allCompleted() {
+      return this.filteredItems.length === this.completedItems.length
+    },
+    filteredCategories() {
+      if (this.isGrouped) {
+        return [...new Set(this.filteredItems.map(i => i.category))]
+      }
+      return ['all']
+    },
+    filteredItems() {
+      let items = this.items.slice()
+      if (this.searchText.length > 0) {
+        items = items.filter(item => item.name.toLowerCase().includes(this.searchText.toLowerCase()))
+      }
+      if (this.hideCompleted) {
+        return items.filter(i => i.state === "open")
+      }
+      return items
+    },
+    completedItems() {
+      return this.filteredItems.filter(item => item.state === "completed")
+    },
+    isSettingsModal() {
+      return this.modalType === "settings"
+    },
+    hasSuggestions() {
+      return this.suggestedCategories.length > 0
+    },
+    isEditItemModeUpdate() {
+      return this.editItemMode === "update"
+    }
+  },
+  watch: {
+    hideCompleted(val) {
+      localStorage.setItem("hideCompleted", val)
+    },
+    isGrouped(val) {
+      localStorage.setItem("isGrouped", val)
+    }
+  },
+  methods: {
+    clearSearch() {
+      this.searchText = ""
+    },
+    showCompleted() {
+      this.hideCompleted = false
+    },
+    showSettingsModal() {
+      this.isModalShown = true
+      this.modalType = "settings"
+      this.modalTitle = "Настройки"
+    },
+    showAddModal() {
+      this.isModalShown = true
+      this.modalTitle = "Добавить"
+      this.editItemMode = "add"
+    },
+    showEditModal(item) {
+      this.isModalShown = true
+      this.modalTitle = "Изменить"
+      this.editItemName = item.name
+      this.editItemCategory = item.category
+      this.editItemMode = "update"
+      this.editItemId = item.id
+    },
+    closeModal() {
+      this.isModalShown = false
+      this.modalType = ""
+      if (this.editItemMode === "edit") {
+        this.editItemMode = "add"
+      }
+      this.editItemId = null
+      this.editItemName = ""
+      this.editItemCategory = ""
+    },
+    modalBackdrop(e) {
+      if (!e.target.classList.contains('modal')) {
+        return
+      }
+      this.closeModal()
+    },
+    itemsByCategory(category) {
+      if (!this.isGrouped) {
+        return this.filteredItems
+      }
+      return this.filteredItems.filter(i => i.category === category)
+    },
+    suggestCategories() {
+      let val = this.editItemCategory
+      if (val.length < 2) {
+        return []
+      }
+      let allCategories = [...new Set(this.items.map(i => i.category))]
+      this.suggestedCategories = allCategories.filter(c => c.toLowerCase().includes(val.toLowerCase()))
+    },
+    setCategory(category) {
+      this.editItemCategory = category
+      this.suggestedCategories = []
+    },
+    toggleHideEmptyCategories() {
+      if (!this.hideCompleted) {
+        hideEmptyCategories = false
+      }
+    },
+    async toggle(item) {
+      if (item.is_checked) {
+        item.is_prechecked = true;
+        item.timer = setTimeout(async () => {
+          await this.toggleRequest(item);
+        }, 1500);
+      } else {
+        if (item.is_prechecked) {
+          clearTimeout(item.timer);
+          item.is_prechecked = false;
+          return;
+        }
+        await this.toggleRequest(item);
+      }
+    },
+    async toggleRequest(item) {
+      let res = await fetch(`/toggleitem?id=${item.id}`, {headers: {'X-WS-Client-ID': clientID}})
+      let data = await res.json()
+      item.is_checked = data.is_checked
+      if (item.is_checked) {
+        item.state = "completed"
+      } else {
+        item.state = "open"
+      }
+      item.is_prechecked = false
+    },
+    async removeItem() {
+      let res = await fetch(
+        `/deleteitem?id=${this.editItemId}`, 
+        {headers: {'X-WS-Client-ID': clientID}}
+        )
+      if (!res.ok) {
+        this.editItemError = `${res.status} ${res.statusText}`
+        return
+      }
+      let idx = this.items.findIndex(i => i.id === this.editItemId)
+      this.items.splice(idx, 1)
+      this.closeModal()
+    },
+    async addItem() {
+      let res = await fetch(
+        `/additem?name=${this.editItemName}&category=${this.editItemCategory}`, 
+        {headers: {'X-WS-Client-ID': clientID}}
+        )
+      if (!res.ok) {
+        this.editItemError = `${res.status} ${res.statusText}`
+        return
+      }
+      data = await res.json()
+      this.items.push(Object.assign(data, {state: "open"}))
+      this.closeModal()
+    },
+    async updateItem() {
+      let res = await fetch(
+        `/edititem?id=${this.editItemId}&name=${this.editItemName}&category=${this.editItemCategory}`, 
+        {headers: {'X-WS-Client-ID': clientID}}
+        )
+      if (!res.ok) {
+        this.editItemError = `${res.status} ${res.statusText}`
+        return
+      }
+      let idx = this.items.findIndex(i => i.id === this.editItemId)
+      this.items[idx].name = this.editItemName
+      this.items[idx].category = this.editItemCategory
+      this.closeModal()
+    }
+  },
+  async mounted() {
+    let res = await fetch('/items', {headers: {'X-WS-Client-ID': clientID}})
+    let rawItems = await res.json()
+    for (let item of rawItems) {
+      let state = "open";
+      if (item.is_checked) {
+        state = "completed";
+      }
+      this.items.push({ ...item, state: state })
+    }
+
+    // Load settings from local storage, if present
+    let hideCompletedLocalStorage = localStorage.getItem("hideCompleted")
+    if (hideCompletedLocalStorage !== null) {
+      this.hideCompleted = JSON.parse(hideCompletedLocalStorage)
+    }
+    let isGroupedLocalStorage = localStorage.getItem("isGrouped")
+    if (isGroupedLocalStorage !== null) {
+      this.isGrouped = JSON.parse(isGroupedLocalStorage)
+    }
+
+    // websocket events mapping
+    socket.addEventListener('open', function(e) {console.log(e)})
+    socket.addEventListener('message', raw => {
+      let event = JSON.parse(raw.data)
+      let idx = null
+      switch (event.type) {
+        case "toggle":
+        idx = this.items.findIndex(i => i.id === event.data.id)
+        let state = "open";
+        if (event.data.is_checked) {
+          state = "completed";
+        }
+        this.items[idx].is_checked = event.data.is_checked
+        this.items[idx].state = state
+        break;
+        case "edit":
+        idx = this.items.findIndex(i => i.id === event.data.id)
+        this.items[idx].name = event.data.name
+        this.items[idx].category = event.data.category
+        break;
+        case "delete":
+        idx = this.items.findIndex(i => i.id === event.data.id)
+        this.items.splice(idx, 1)
+        break;
+        case "add":
+        this.items.push(Object.assign(event.data, {state: "open"}))
+      }
+    })
+    this.loading = false
+  }
+})
+
+app.mount('#app');
